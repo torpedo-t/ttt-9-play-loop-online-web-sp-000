4a8ae46839ff9f9ddfb958f651d426a79aa0e44d
Thomas John Helin <thomashelin@yahoo.com>
2020-03-25 14:43:54 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -26,7 +26,7 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
-  index = input_to_index(user_input)
+  index = input_to_index(input)
   if valid_move?(board, index)
     move(board, index)
     display_board(board)
